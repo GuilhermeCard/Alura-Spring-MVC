c1cdeddf362e2efbab48f68dab266906470911f6
Guilherme Cardoso <guilherme.cardoso@compasso.com.br>
2021-08-08 15:11:55 -0300

    Integrando Spring Data

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,34 +1,26 @@
 package br.com.alura.mvc.mudi.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
 	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
 	@GetMapping(value = "/home")
 	public String home(Model model) {
-		
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Xiaomi Redmi Note 8");
-		pedido.setUrlImagem("https://carrefourbr.vtexassets.com/arquivos/ids/7109498-1200-auto?width=1200&height=auto&aspect=true");
-		pedido.setUrlProduto("https://www.carrefour.com.br/smartphone-xiaomi-note-8-64gb-moonlight-white-global-mp28480051/p?utm_medium=sem&utm_source=google_pla_3p&utm_campaign=nata&gclid=CjwKCAjw3riIBhAwEiwAzD3TiaMQAf2_0qnQqUJW1_UVi0xN0qT5BmIa064D7efLwJ0k6-zoIesaiRoC1CUQAvD_BwE");
-		pedido.setDescricao("uma descrição qualquer para esse produto");
-		
-		List<Pedido> pedidos = Arrays.asList(pedido);
-		
+		List<Pedido> pedidos = pedidoRepository.findAll();
 		model.addAttribute("pedidos", pedidos);
-		
-		
-		
 		return "home";
 	}
-	
 
 }
