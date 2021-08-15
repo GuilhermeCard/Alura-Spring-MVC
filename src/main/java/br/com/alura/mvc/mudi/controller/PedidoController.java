4406b295caf4402f6caf27a1f53d243ba8b3f766
Guilherme Cardoso <guilherme.cardoso@compasso.com.br>
2021-08-15 19:34:19 -0300

    Aulas: Autenticação com Spring Security e Provedor de Autenticação

--- src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -3,6 +3,7 @@ package br.com.alura.mvc.mudi.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,14 +12,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.alura.mvc.mudi.dto.RequisicaoNovoPedido;
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.model.User;
 import br.com.alura.mvc.mudi.repository.PedidoRepository;
+import br.com.alura.mvc.mudi.repository.UserRepository;
 
 @Controller
 @RequestMapping("pedido")
 public class PedidoController {
 	
 	@Autowired
-	PedidoRepository pedidoRepository;
+	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
 
 	@GetMapping("formulario")
 	public String formulario(RequisicaoNovoPedido requisicao) {
@@ -32,6 +38,10 @@ public class PedidoController {
 		}
 
 		Pedido pedido = requisicao.toPedido();
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+		
+		User user = userRepository.findByUsername(username);
+		pedido.setUser(user);		
 		pedidoRepository.save(pedido);
 		return "redirect:/home";
 
